21193ff826e5a56e97161ba93309395338ee37ec
NPilis <norbertpilis@gmail.com>
2020-08-18 12:49:03 +0200

    Changed project structure for frontend with create-react-app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './App.css';
+
+class App extends Component {
+
+  render () {
+    return (
+      <div className="App">
+        <h1>Test2</h1>
+      </div>
+    );
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Test</h1>
+//     </div>
+//   );
+// }
+
+export default App;
