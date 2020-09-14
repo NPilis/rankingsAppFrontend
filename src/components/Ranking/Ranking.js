819f4059a4e9cfa0581bbe0ed8428fa1bf111bc4
NPilis <norbertpilis@gmail.com>
2020-09-14 21:53:34 +0200

    Styling and testing some things..

--- src/components/Ranking/Ranking.js
+++ src/components/Ranking/Ranking.js
@@ -1,35 +1,80 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import RankingContent from '../RankingContent/RankingContent';
 import RankingInteractions from '../RankingInteractions/RankingInteractions';
 import RankingMedia from '../RankingMedia/RankingMedia';
+import axios from 'axios';
+import {connect} from 'react-redux';
 
 import cls from './Ranking.module.css'
 
-export default (props) => {
+class Ranking extends Component {
+
+    likeRanking = (event) => {
+        event.preventDefault();
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
     
-    return(
-        <div className={cls.Ranking} onClick={props.clicked}>
-            <div className={cls.FlexRow}>
-                <RankingContent
-                    title={props.rank.title}
-                    ranking_positions={props.rank.top_three_rp}/>
-                <RankingInteractions
-                    likes={props.rank.likes}
-                    dislikes={props.rank.dislikes}
-                    // shares={props.rank.shares}
-                    // comments={props.rank.comments}
+        if (this.props.token) {
+            config.headers['Authorization'] = `Token ${this.props.token}`;
+        }
+    
+        console.log(this.props.user)
+        axios.post('/api/rankings/'+ this.props.rank.uuid + '/like/', null, config)
+             .then(res => {
+                 console.log(res);
+             })
+             .catch(err => {
+                 console.log(err.response);
+             })
+    }
+
+    render() {
+
+        return (
+            <div className={cls.Ranking} onClick={this.props.clicked}>
+                <div className={cls.FlexRow}>
+                    <RankingContent
+                        title={this.props.rank.title}
+                        ranking_positions={this.props.rank.top_three_rp} />
+                    <RankingInteractions
+                        user={this.props.user}
+                        likes={this.props.rank.likes}
+                        dislikes={this.props.rank.dislikes}
+                        shares={this.props.rank.shares}
+                        comments={this.props.rank.comments}
+                        liked={this.likeRanking}
                     >
-                    Likes, comments, shares
+                        Likes, comments, shares
                 </RankingInteractions>
-            </div>
-            <div className={cls.FlexCol}>
-                <RankingMedia
-                    author={props.rank.author}
-                    created_at={props.rank.created_at}>
-                    Images, author, additional information
+                </div>
+                <div className={cls.FlexCol}>
+                    <RankingMedia
+                        author={this.props.rank.author}
+                        created_at={this.props.rank.created_at}>
+                        Images, author, additional information
                 </RankingMedia>
+                </div>
+
             </div>
-        </div>
-    );
+        );
+    }
 }
+
+const mapStateToProps = (state) => ({
+    isAuthenticated: state.auth.isAuthenticated,
+    token: state.auth.token,
+    user: state.auth.user
+});
+
+const mapDispatchToProps = dispatch => {
+    return {
+
+    };
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Ranking);
